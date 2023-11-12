af4401f1d75fe8d8756241b602ae9d23755b7cfb
Iñigo Gil <inigogil@bilbokoeskolapioak.org>
2023-11-12 17:19:12 +0100

    Observer bigarren partea

--- src/observer/Main.java
+++ src/observer/Main.java
@@ -13,6 +13,17 @@ public class Main {
 		Observable pacient = new Covid19Pacient("inigo", 22);
 		new PacientObserverGUI(pacient);
 		new PacientSymptomGUI(pacient);
+		new PacientThermometerGUI(pacient);
+		
+		Observable secondPatient = new Covid19Pacient("Jon", 30);
+		new PacientObserverGUI(secondPatient);
+		new PacientSymptomGUI(secondPatient);
+		new PacientThermometerGUI(secondPatient);
+		
+		Observable thirdPatient = new Covid19Pacient("Ekain", 26);
+		new PacientObserverGUI(thirdPatient);
+		new PacientSymptomGUI(thirdPatient);
+		new PacientThermometerGUI(thirdPatient);
 	}
 
 
