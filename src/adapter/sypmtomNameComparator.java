99a1f01bc2523613af93d80ba1e5a3fa3c4463d4
Danel0230 <osaetxebe@gmail.com>
2023-11-12 19:42:43 +0100

    comparator

---
+++ src/adapter/sypmtomNameComparator.java
@@ -0,0 +1,15 @@
+package adapter;
+
+import java.util.Comparator;
+
+import domain.Symptom;
+public class sypmtomNameComparator implements Comparator<Symptom>{
+
+		@Override
+		public int compare(Symptom o1, Symptom o2) {
+			return o1.getName().compareToIgnoreCase(o2.getName());
+		}
+
+	}
+
+}
