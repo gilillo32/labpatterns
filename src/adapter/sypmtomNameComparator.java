be183f7a762ea60fff6ea1e70bc0d333a4739d88
Danel0230 <osaetxebe@gmail.com>
2023-11-12 19:43:27 +0100

    Update sypmtomNameComparator.java

--- src/adapter/sypmtomNameComparator.java
+++ src/adapter/sypmtomNameComparator.java
@@ -5,7 +5,6 @@ import java.util.Comparator;
 import domain.Symptom;
 public class sypmtomNameComparator implements Comparator<Symptom>{
 
-		@Override
 		public int compare(Symptom o1, Symptom o2) {
 			return o1.getName().compareToIgnoreCase(o2.getName());
 		}
