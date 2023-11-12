5082399d31b748e526e531e1f650b531058a1ee6
Danel0230 <osaetxebe@gmail.com>
2023-11-12 20:13:30 +0100

    adapter

--- src/adapter2/Main.java
+++ src/adapter2/Main.java
@@ -11,10 +11,20 @@ public class Main {
 		pacient.addSymptomByName("cefalea", 1);
 		pacient.addSymptomByName("astenia", 3);
 		
+		Covid19Pacient pacient1=new Covid19Pacient("jon", 35);
+		
+		pacient1.addSymptomByName("disnea", 3);
+		pacient1.addSymptomByName("cefalea", 3);
+		pacient1.addSymptomByName("astenia", 3);
+		
 		ShowPacientTableGUI gui=new ShowPacientTableGUI(pacient);
 		gui.setPreferredSize(
 			      new java.awt.Dimension(300, 200));
 		gui.setVisible(true);
+		ShowPacientTableGUI gui1=new ShowPacientTableGUI(pacient1);
+		gui1.setPreferredSize(
+			      new java.awt.Dimension(300, 200));
+		gui1.setVisible(true);
 
 
 	}
